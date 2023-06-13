d5d819a3d2eddacc9cd5310ad07db678f611ccde
Mario Henrique <132026495+MARIO-HENRIQUE-DEV@users.noreply.github.com>
2023-06-13 13:24:16 -0300

    Add files via upload

--- jogo/script.js
+++ jogo/script.js
@@ -1,22 +1,25 @@
 const andando = document.querySelector('.andando');
 const estacionado = document.querySelector('.estacinado');
-
+const audio =document.querySelector('audio');
 const jump = () => {
     andando.classList.add('jump');/*sapo*/
+    audio.play();
 
     setTimeout(() => {
-        andando.classList.remove('jump');/*sapo*/
+        andando.classList.remove('jump',);/*sapo*/
+                
 
     }, 700);
 
 }
+
 
 const loop = setInterval(() => {
     
     const estacionadoPosition = estacionado.offsetLeft;/*jacare*/
     const andandoPosition = +window.getComputedStyle(andando).bottom.replace('px','');/* sapo*/
 
-    if (estacionadoPosition <= 40 && estacionadoPosition > -2  && andandoPosition < 150) { /*jacare*/
+    if (estacionadoPosition <= 20 && estacionadoPosition > -2  && andandoPosition < 50) { /*jacare*/
 
         estacionado.style.animation = 'none';/*jacare*/
         estacionado.style.left =`${estacionadoPosition}px`;/*jacare*/
